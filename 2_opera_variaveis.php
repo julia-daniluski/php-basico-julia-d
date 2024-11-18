2476df58d00f659387035a50e247a2ce097529f2
Julia Daniluski <julia.o.daniluski@gmail.com>
2024-11-18 13:58:37 -0300

    2 e 3

--- 2_opera_variaveis.php
+++ 2_opera_variaveis.php
@@ -1,1 +1,33 @@
-<!-- digitar o programa -->
+ <?php
+// Recebe os dois valores pela URL usando o método GET
+// Digitar a URL para poder enviar os valores
+//Exemplo de URL: http://localhost/php-basico-Alunos/2_opera_variaveis.php?numero1=10&numero2=5
+
+$numero1 = $_GET['numero1'];
+$numero2 = $_GET['numero2'];
+
+// Verifica se os valores foram passados corretamente
+if (isset($numero1) && isset($numero2)) {
+// Converte os valores para inteiros
+$numero1 = (int)$numero1; 
+$numero2 = (int)$numero2; 
+
+// SOMAR
+$soma = $numero1 + $numero2;
+//SUBTRAIR
+$subtracao = $numero1 - $numero2;
+//MULTIPLICAÇÃO
+$multiplicacao = $numero1 * $numero2;
+//DIVISAO
+$divisao = $numero1 / $numero2;
+
+// Exibir os resultados
+echo "A soma é: $soma <br>";
+echo "A subtração é: $subtracao <br>";
+echo "A multiplicação é: $multiplicacao <br>";
+echo "A divisão é: $divisao <br>";
+
+} else {
+ echo "ATENÇÃO! Por favor, forneça os valores de numero1 e numero2 pela URL.";
+}
+?>
--- 3_formulario.php
+++ 3_formulario.php
@@ -1,1 +1,41 @@
-<!-- digitar o programa/código -->
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Exemplo de formulário de Cadastro</title>
+</head>
+<body>
+<!-- Método pode ser POST ou GET -->
+<form method="post" action="">
+
+    <label for="nome">Nome: </label>
+    <input type="text" name="nome" required><br>
+
+    <label for="email">Email:</label>
+    <input type="email" name="email" required><br>
+
+    <label for="telefone">Telefone: </label>
+    <input type="text" name="telefone" required><br>
+
+    <button type="submit">Enviar</button>
+</form>
+
+<!-- Parte do programa em PHP -->
+<?php
+
+// Verifica se o formulário foi enviado
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Recebe os valores enviados pelo formulário
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $telefone = $_POST['telefone']; // Corrigido: Adicionando o sinal de '='
+
+    // Exibe os valores recebidos
+    echo "<h2>Dados Recebidos:</h2>";
+    echo "Nome: $nome <br>";
+    echo "Email: $email <br>";
+    echo "Telefone: $telefone <br>";
+}
+?>
+</body>
+</html>
