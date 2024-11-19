92112a3b7e0ac43ba541b4a5294bdfb5b7c02a8c
Julia Daniluski <julia.o.daniluski@gmail.com>
2024-11-19 09:11:49 -0300

    15

--- 15c_logout.php
+++ 15c_logout.php
@@ -1,8 +1,11 @@
 <?php
 // Página de logout (15c_logout.php)
+session_start();
 
+// Destroi a sessão do usuário
+session_destroy();
 
-// Digitar PHP (1º Aqui)
-
-
+// Redireciona para a página de login ou para o sistema
+header("Location: 15a_sistema.php");
+exit();
 ?>
